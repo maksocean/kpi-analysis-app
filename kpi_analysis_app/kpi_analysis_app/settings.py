5f2f5c42c5d6bf65d3228f027c7cf7a06b53a989
Maksim Inozemtsev <maksim.inozemtsev@gmail.com>
2026-03-05 16:20:40 +0500

    feat: configure static files settings (STATIC_URL, STATIC_ROOT)

--- kpi_analysis_app/kpi_analysis_app/settings.py
+++ kpi_analysis_app/kpi_analysis_app/settings.py
@@ -131,4 +131,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# URL, по которому будут доступны статические файлы в браузере
+STATIC_URL = '/static/'
+
+# Папка, куда будет собирать статику команда collectstatic (для продакшена)
+STATIC_ROOT = BASE_DIR / 'staticfiles'

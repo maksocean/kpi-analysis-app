dd3a2f1332d7b41694844408ef5640e56e8c84fd
Maksim Inozemtsev <maksim.inozemtsev@gmail.com>
2026-05-05 09:54:52 +0500

    feat: add input validation in serializer

--- kpi_analysis_app/simulator/serializers.py
+++ kpi_analysis_app/simulator/serializers.py
@@ -8,8 +8,15 @@ class ProductionDataSerializer(serializers.ModelSerializer):
 
 class KPICalculatorInputSerializer(serializers.Serializer):
     """Сериализатор для входных данных симулятора (можно не привязывать к модели)."""
-    ore_volume = serializers.FloatField(min_value=0)
-    operating_time = serializers.FloatField(min_value=0)
+    ore_volume = serializers.FloatField(min_value=0.01)
+    operating_time = serializers.FloatField(min_value=0.01)
     downtime = serializers.FloatField(min_value=0)
     crew_size = serializers.IntegerField(min_value=1)
     fuel_consumption = serializers.FloatField(min_value=0)
+
+    def validate(self, data):
+        if data['downtime'] >= data['operating_time']:
+            raise serializers.ValidationError(
+                "Время простоя не может быть больше или равно плановому времени работы"
+            )
+        return data
